be959767ae101f662194a80cdab2c53287686e30
arcenioCorteis <arcenioCorteis@github.com>
2024-12-28 09:50:03 +0200

    feat: create and finish order

---
+++ orders/forms.py
@@ -0,0 +1,29 @@
+from django import forms
+from .models import Order
+
+class OrderCreateForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'telephone',
+            'address',
+            'postal_code',
+            'city',
+            'note',
+            'transport'
+        ]
+        
+        widgets = {
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'telephone': forms.TextInput(attrs={'class': 'form-control'}),
+            'address': forms.TextInput(attrs={'class': 'form-control'}),
+            'postal_code': forms.TextInput(attrs={'class': 'form-control'}),
+            'city': forms.TextInput(attrs={'class': 'form-control'}),
+            'note': forms.Textarea(attrs={'class': 'form-control'}),
+            'transport': forms.RadioSelect,
+        }
